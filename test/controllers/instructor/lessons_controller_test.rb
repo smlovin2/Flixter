417c6e6893d8868d666324a61b34f9f370ff57d4
Sean Lovinger <lovingersean01@gmail.com>
2015-12-30 20:50:19 +0000

    Added permissions to creating lessons. Also did some refactoring of the lessons controller

--- test/controllers/instructor/lessons_controller_test.rb
+++ test/controllers/instructor/lessons_controller_test.rb
@@ -2,15 +2,40 @@ require 'test_helper'
 
 class Instructor::LessonsControllerTest < ActionController::TestCase
   test "new" do
-    section = FactoryGirl.create(:section)
+    user = FactoryGirl.create(:user)
+    sign_in user
+
+    course = FactoryGirl.create(:course, :user => user)
+    section = FactoryGirl.create(:section, :course => course)
     get :new, :section_id => section.id
     assert_response :success
   end
 
-  test "create" do
+  test "new not signed in" do
+    section = FactoryGirl.create(:section)
+    get :new, :section_id => section.id
+    assert_redirected_to new_user_session_path
+  end
+
+  test "new wrong user" do
+    user1 = FactoryGirl.create(:user)
+    sign_in user1
+
     section = FactoryGirl.create(:section)
 
-    assert_difference 'Lesson.count' do
+    get :new, :section_id => section.id
+
+    assert_response :unauthorized
+  end
+
+  test "create" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+
+    course = FactoryGirl.create(:course, :user => user)
+    section = FactoryGirl.create(:section, :course => course)
+
+    assert_difference 'section.lessons.count' do
       post :create, :section_id => section.id, :lesson => {
         :title => 'Lesson 1',
         :subtitle => 'The first lesson',
@@ -19,5 +44,19 @@ class Instructor::LessonsControllerTest < ActionController::TestCase
     assert_redirected_to instructor_course_path(section.course)
 
     assert_equal 1, section.lessons.count
+  end
+
+  test "create wrong user" do
+    user1 = FactoryGirl.create(:user)
+    sign_in user1
+
+    section = FactoryGirl.create(:section)
+
+    post :create, :section_id => section.id, :lesson => {
+      :title => 'Lesson 1',
+      :subtitle => 'The first lesson',
+    }
+
+    assert_response :unauthorized
   end
 end
