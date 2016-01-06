09ac8bbce423ad5a04e9d6c393901fb7de6fe33d
lovin2 <lovingersean01@gmail.com>
2016-01-06 05:21:45 +0000

    Added student enrollment

---
+++ test/controllers/enrollments_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class EnrollmentsControllerTest < ActionController::TestCase
+  test "Trying to enroll when not signed in" do
+    course = FactoryGirl.create(:course)
+    
+    post :create, :course_id => course.id 
+    assert_redirected_to new_user_session_path
+  end
+  
+  test "create" do
+    course = FactoryGirl.create(:course)
+    sign_in course.user
+    
+    assert_difference 'Enrollment.count' do
+      post :create, :course_id => course.id, :enrollment => {
+        :course_id => course.id,
+        :user_id => course.user.id
+      }
+      
+      assert_redirected_to course_path(course)
+    end
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "Enrolled" do
+    enrollment = FactoryGirl.create(:enrollment)
+    
+    assert_equal true, enrollment.user.enrolled_in?(enrollment.course)
+  end
+  
+  test "Not Enrolled" do
+    course = FactoryGirl.create(:course)
+    
+    assert_equal false, course.user.enrolled_in?(course)
+  end
 end
