4821879d75e1decc7188af70505d9e23dbdd1578
lovin2 <lovingersean01@gmail.com>
2016-01-08 06:41:20 +0000

    Added next lesson button

--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class LessonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get next lesson" do
+    lesson1 = FactoryGirl.create(:lesson)
+    lesson2 = FactoryGirl.create(:lesson, :section => lesson1.section)
+    
+    assert_equal lesson2, lesson1.next_lesson
+  end
+  
+  test "get next lesson in next session" do
+    section1 = FactoryGirl.create(:section)
+    section2 = FactoryGirl.create(:section, :course => section1.course)
+    lesson1 = FactoryGirl.create(:lesson, :section => section1)
+    lesson2 = FactoryGirl.create(:lesson, :section => section2)
+    
+    assert_equal lesson2, lesson1.next_lesson
+  end
+  
+  test "no next lesson" do
+    lesson = FactoryGirl.create(:lesson)
+    assert_equal nil, lesson.next_lesson
+  end
 end
--- test/models/section_test.rb
+++ test/models/section_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class SectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get next section" do
+    section1 = FactoryGirl.create(:section)
+    section2 = FactoryGirl.create(:section, :course => section1.course)
+    
+    assert_equal section2, section1.next_section
+  end
+  
+  test "no next section" do
+    section = FactoryGirl.create(:section)
+    assert_equal nil, section.next_section
+  end
 end
